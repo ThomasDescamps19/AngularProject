da13b390d1799e0b375d132053c00189f4aee2cd
ThomasDescamps19 <thomasdescamps19@gmail.com>
2023-11-23 17:14:58 +0100

    add color

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -15,6 +15,43 @@ export class AppComponent implements OnInit {
   title = 'Listes de pokémons';
   pokemonList: Pokemon[] = POKEMONS;
 
+  getTypeColor(pokemon: Pokemon): string {
+    const types = pokemon.types;
+
+    if (types) {
+      if (types.includes('Plante') && types.includes('Poison')) {
+        // Si le Pokémon a les deux types 'Feu' et 'Plante', retournez une couleur spécifique
+        return '#809980';
+      }
+      if (types.includes('Normal') && types.includes('Vol')) {
+        // Si le Pokémon a les deux types 'Feu' et 'Plante', retournez une couleur spécifique
+        return 'orange';
+      }
+
+      // Ajoutez des conditions supplémentaires pour d'autres combinaisons de types si nécessaire...
+
+      // Sinon, retournez la couleur du premier type
+      if (types.length > 0) {
+        const firstType = types[0].toLowerCase();
+        switch (firstType) {
+          case 'feu':
+            return 'red';
+          case 'plante':
+            return 'green';
+          case 'eau':
+            return '#add8e6';
+          case 'normal':
+            return 'olive';
+          // Ajoutez d'autres cas au besoin...
+          default:
+            return '#ccc'; // Couleur par défaut
+        }
+      }
+    }
+
+    return '#ccc'; // Aucune correspondance, couleur par défaut
+  }
+
   ngOnInit() {
     console.table(this.pokemonList);
     this.selectPokemon(this.pokemonList[5]);
