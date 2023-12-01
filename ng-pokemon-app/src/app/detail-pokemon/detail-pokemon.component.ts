c95781fb2dd49d8a17a9abde6e148e8d5e206e95
ThomasDescamps19 <thomasdescamps19@gmail.com>
2023-12-01 15:35:04 +0100

    new component

--- ng-pokemon-app/src/app/detail-pokemon/detail-pokemon.component.ts
+++ ng-pokemon-app/src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,34 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { POKEMONS } from '../mock-pokemons-list';
 import { Pokemon } from '../pokemon';
 import { CommonModule } from '@angular/common';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DetailPokemonComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    DetailPokemonComponent,
+    PokemonTypeColorPipe,
+  ],
   styleUrls: ['./detail-pokemon.component.css'],
-  templateUrl: './detail-pokemon.component.html'
+  templateUrl: './detail-pokemon.component.html',
 })
-
 export class DetailPokemonComponent implements OnInit {
-
   pokemonList: Pokemon[];
-  pokemon: Pokemon|undefined;
-  
-  constructor(private route: ActivatedRoute) {}
-    
+  pokemon: Pokemon | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
   ngOnInit() {
     this.pokemonList = POKEMONS;
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
 
     if (pokemonId) {
- 
-      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+      this.pokemon = this.pokemonList.find(
+        (pokemon) => pokemon.id == +pokemonId
+      );
     } else {
       this.pokemon = undefined;
     }
-   
+  }
+
+  goBack() {
+    this.router.navigate(['/pokemons']);
   }
 }
--- ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
+++ ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemons-list';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { BorderCardDirective } from '../border-card.directive';
 import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 import { DetailPokemonComponent } from '../detail-pokemon/detail-pokemon.component';
@@ -10,15 +10,26 @@ import { DetailPokemonComponent } from '../detail-pokemon/detail-pokemon.compone
 @Component({
   selector: 'app-list-pokemon',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BorderCardDirective, PokemonTypeColorPipe,ListPokemonComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+  ],
   templateUrl: './list-pokemon.component.html',
-  styleUrl: './list-pokemon.component.css'
+  styleUrl: './list-pokemon.component.css',
 })
 export class ListPokemonComponent {
   pokemonList: Pokemon[] = POKEMONS;
   pokemonSelected: Pokemon | undefined;
   Message: string = '';
 
+  constructor(private router: Router) {}
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
+  }
 
   selectPokemon(pokemon: Pokemon) {
     this.Message = `Vous avez cliqué sur le Pokémon ${pokemon.name}`;
