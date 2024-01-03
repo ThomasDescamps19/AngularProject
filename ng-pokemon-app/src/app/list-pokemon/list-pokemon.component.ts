5edfc4d63cb19105c292e4daf4a791a20ba91089
ThomasDescamps19 <thomasdescamps19@gmal.com>
2024-01-03 16:07:14 +0100

    maj

--- ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
+++ ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../pokemon';
-import { POKEMONS } from '../mock-pokemons-list';
+// import { POKEMONS } from '../mock-pokemons-list';
 import { Router, RouterOutlet } from '@angular/router';
 import { BorderCardDirective } from '../border-card.directive';
 import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
-import { DetailPokemonComponent } from '../detail-pokemon/detail-pokemon.component';
+// import { DetailPokemonComponent } from '../detail-pokemon/detail-pokemon.component';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -20,12 +21,19 @@ import { DetailPokemonComponent } from '../detail-pokemon/detail-pokemon.compone
   templateUrl: './list-pokemon.component.html',
   styleUrl: './list-pokemon.component.css',
 })
-export class ListPokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
+
   pokemonSelected: Pokemon | undefined;
   Message: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private pokemonService: PokemonService) 
+    { }
+
+    ngOnInit(){
+      this.pokemonList = this.pokemonService.getPokemonList();
+    } 
 
   goToPokemon(pokemon: Pokemon) {
     this.router.navigate(['/pokemon', pokemon.id]);
